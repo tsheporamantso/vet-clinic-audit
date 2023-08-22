d4abc516580a1de26a44cb549406773fdab1e870
tsheporamantso <tshepo.ramantso@outlook.com>
2023-08-22 14:09:48 +0200

    Add tables with constrains

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,43 @@
+CREATE TABLE patients(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    date_of birth DATE,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE medical_histories(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_history_id INT REFERENCES medical_histories(id),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE treatments(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR,
+    name VARCHAR,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price DECIMAL,
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id),
+    PRIMARY KEY(id)
+);
+
+
+
